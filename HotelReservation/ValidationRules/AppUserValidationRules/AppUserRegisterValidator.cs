fca008d36dfa334ac048622501e032331207d0bf
fkoca <ferdikoca12@gmail.com>
2024-01-17 00:20:40 +0300

    add fluent validator to AppUserRegisterDTO

---
+++ HotelReservation/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using HotelReservation.DTOs.AppUserDTOs;
+
+namespace HotelReservation.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator : AbstractValidator<AppUserRegisterDTO>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez.");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyisim alanı boş geçilemez.");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adı alanı boş geçilemez.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez.");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Parola alanı boş geçilemez.");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Parola doğrulama alanı boş geçilemez.");
+
+            RuleFor(x => x.UserName).MinimumLength(2).WithMessage("Lütfen en az 2 karakter Kullanıcı adı giriniz.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Lütfen geçerli bir email adresi giriniz.");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalarınız uyuşmuyor.");
+        }
+    }
+}
