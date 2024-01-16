50621bdf1cf44f944874ca7a782ae83fbb16cae2
fkoca <ferdikoca12@gmail.com>
2024-01-17 00:14:11 +0300

    add appuserDto class

---
+++ HotelReservation/DTOs/AppUserDTOs/AppUserRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelReservation.DTOs.AppUserDTOs
+{
+    public class AppUserRegisterDTO
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+}
